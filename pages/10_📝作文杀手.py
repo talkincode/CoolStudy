2f4b3bce4f881a8ee1dbeaed123b8ab16e125cdd
Jett Wang <jamiesun.net@gmail.com>
2023-12-12 10:36:04 +0800

    Add new page for "作文杀手" feature
    为“作文杀手”功能创建了一个新页面，旨在帮助用户以新颖、引人入胜和合乎逻辑的方式撰写论文。该界面接受两个用户输入：论文主题和写作要求，然后根据这些输入生成内容。

---
+++ pages/10_📝作文杀手.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import sys
+import os
+from dotenv import load_dotenv
+from libs.llms import openai_streaming
+
+sys.path.append(os.path.abspath('..'))
+load_dotenv()
+
+st.markdown("## 📝 作文杀手")
+st.markdown("教你用一种新的方式写作文，让你的作文更加生动有趣，更加有逻辑。")
+
+topic = st.text_input("输入作文题目：", "我的梦想")
+remark = st.text_area("写作要求：", "我的梦想是当一名科学家。")
+if st.button("开始写作"):
+    with st.spinner("生成中..."):
+        msg = f"""
+        请按照我的要求写一篇中学生的作文。
+        - 语言风格要符合中学生的特点；
+        - 六百到七百字；
+        - 适当引用诗词，成语，谚语；
+        - 弄清用户输入题材;
+        - 开头简洁，中间内容丰富，不能有废话，结尾点题，呼应标题，总结全文;
+        
+        写作主题；{topic}
+        写作要求：{remark}
+        作文内容："""
+        response = openai_streaming(msg,[])
+        placeholder = st.empty()
+        full_response = ''
+        for item in response:
+            text = item.content
+            if text is not None:
+                full_response += text
+                placeholder.markdown(full_response)
+        placeholder.markdown(full_response)
+
