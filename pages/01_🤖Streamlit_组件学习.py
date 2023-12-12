07a42090e545e6e7c43b96a61dc935bc30ae2fd3
boyjiangboyu <boyjiangboyu@geamil.com>
2023-12-12 22:15:49 +0800

    修改

--- pages/01_🤖Streamlit_组件学习.py
+++ pages/01_🤖Streamlit_组件学习.py
@@ -19,4 +19,32 @@ with st.expander("文本组件"):
     st.header('副标题文本')
     st.subheader('二级副标题文本')
     st.code('st.code("Hello, Streamlit!") # 代码块')
+    st.write(['st', 'is <', 3])
+    
+    st.latex(r'''公式 \pi''')
+   
 
+
+import streamlit as st
+import time
+chinken = st.number_input("输入黑字", min_value=0, value=0, step=1)
+
+for i in range(chinken):
+    # 创建一个占位符
+    placeholder = st.empty()    
+    # 初始时在占位符位置显示一些文本
+    placeholder.code('st.code("Hello, Streamlit!") # 代码块')
+        # 创建一个占位符
+    placeholder = st.empty()
+    # 等待几秒钟
+    time.sleep(2)
+
+    # 然后更新占位符位置的内容
+    placeholder.text("你干嘛嘿嘿有")
+    time.sleep(2)
+        # 创建一个占位符
+    placeholder = st.empty()
+    # 初始时在占位符位置显示一些文本
+    placeholder.text("鸡你太美")
+        # 创建一个占位符
+    placeholder = st.empty()
